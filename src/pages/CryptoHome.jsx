04805685404fd12b06ac888f8b7cc0bee208d31d
Abha Rani <abha.rangira@gmail.com>
2023-05-26 17:40:40 +0530

    homepage

--- src/pages/CryptoHome.jsx
+++ src/pages/CryptoHome.jsx
@@ -6,12 +6,10 @@ const CryptoHome = () => {
     <div>
       <NavBar />
       <div className=" bg-[#fafbff]   rounded-md mx-20 mt-6 px-8 pt-5 pb-2">
-        <div className=" grid grid-cols-4 gap-3   ">
-          {/* dividing homepagee into two sections */}
-          <div className=" col-span-3 grid grid-rows-3 gap-3  ">
+        <div className=" grid grid-cols-4 gap-3   ">               {/* dividing homepagee into two sections */}
+          <div className=" col-span-3 grid grid-rows-3 gap-3  ">    {/* left Scetion */}
 
-            {/* left Scetion */}
-            <div className="grid grid-cols-6 gap-5 ">
+            <div className="grid grid-cols-6 gap-5 ">           {/*dropdown+searchbar goes here*/}
               <div className=" col-span-1 bg-white shadow-sm rounded-md  w-20">
                 <div> usd V </div>
               </div>
@@ -19,18 +17,21 @@ const CryptoHome = () => {
                 searchbar
               </div>
             </div>
-            <div className="bg-white shadow-sm rounded-md">Chart</div>
-            <div className="grid grid-cols-2 gap-2">
+
+            <div className="bg-white shadow-sm rounded-md">Chart</div>           {/* chart goes here */}
+
+
+            <div className="grid grid-cols-2 gap-2">              {/*  Portfolio+exchange*/}
               <div className="bg-white shadow-sm rounded-md">portfolio
                 {/* <Portfolio /> */}
               </div>
               <div className="bg-white shadow-sm rounded-md "><p className="pr-24">Exchange</p></div>
             </div>
           </div>
-          <div className=" bg-white shadow-sm rounded-md   ">
-            {/* //sidebar section// */}
-            Sidebar
 
+
+          <div className=" bg-white shadow-sm rounded-md   ">            {/* //sidebar section// */}
+            Sidebar
             {/* <Market /> */}
           </div>
         </div>
